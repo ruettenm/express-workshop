404e1ac66b9177f60714ff7620b737da2a2fa62c
Matthias Rütten <matthias.ruetten@codecentric.de>
2021-06-16 11:49:26 +0200

    solutions for exercise 5

--- src/index.ts
+++ src/index.ts
@@ -4,24 +4,34 @@ import morgan from "morgan";
 import { handleHelloGet, handleHelloPost } from "./sayHello";
 import { defaultErrorHandler, handleEveryOtherError } from "./errorHandler";
 import { adminRouter } from "./adminRouter";
+import path from "path";
 
 const app = express();
 const port = 3000;
 
-app.use(express.json())
-app.use(morgan('tiny'))
+app.use(express.json());
+app.use(morgan("tiny"));
+
+app.use("/static", express.static(path.join(__dirname, "assets")));
+app.set("views", path.join(__dirname, "views"));
+app.set("view engine", "ejs");
 
 app.use((req, res, next) => {
   console.log(`Somebody is calling: ${req.url}`);
-  next()
+  next();
 });
 
 app.get("/", (req, res) => {
-  res.send("Hello World!");
+  const imageNumber = Math.floor(Math.random() * 3) + 1;
+  res.render("index", {
+    context: {
+      catImage: `cat${imageNumber}.jpeg`,
+    },
+  });
 });
 
-app.get('/hello', handleHelloGet)
-app.post('/hello', handleHelloPost)
+app.get("/hello", handleHelloGet);
+app.post("/hello", handleHelloPost);
 
 app.use("/admin", adminRouter);
 
--- src/sayHello.ts
+++ src/sayHello.ts
@@ -2,20 +2,16 @@ import { RequestHandler } from "express";
 import { ApiError, NotFoundError } from "./errorHandler";
 
 export const handleHelloGet: RequestHandler = (req, res) => {
-  const name = req.query.name || 'World'
+  const name = req.query.name || "World";
 
-  if (name === 'Marco') {
-    throw new NotFoundError('Marco is not available')
-  }
-  if (name === 'Vincent') {
-    throw new Error('💣')
-  }
-  if (name === 'API') {
-    throw new ApiError(401, 'unauthorized', 'You are not authorized!')
-  }
-
-  res.json({hello: name})
-}
+  const imageNumber = Math.floor(Math.random() * 3) + 1;
+  res.render("index", {
+    context: {
+      catImage: `cat${imageNumber}.jpeg`,
+      name,
+    },
+  });
+};
 
 export const handleHelloPost: RequestHandler = (req, res) => {
   const name = req.body.name || 'World'
